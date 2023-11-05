3e1e3ce9406acd101e5311639df038609e3174e9
菅沼さゆり <>
2023-11-05 14:38:06 +0900

    webpack.config.jsの変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'app', 'index.js'),
+  entry: {
+    main: './src/'
+  },
   output: {
     path: path.resolve(__dirname, 'dist', 'app'),
     filename: 'bundle.js',
