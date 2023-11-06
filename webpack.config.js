f33b14b919e3bed6d00f82193c14fc1ef6f896bd
菅沼さゆり <>
2023-11-06 21:07:36 +0900

    バージョンの修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
 const path = require('path');
 
-
-module.exports = function(api) {
-  api.cache(true);
-
-  const presets = [
-    [`@babel/preset-env`, { 'useBuiltIns': `usage`, 'corejs': `2` }],
-    [`@babel/preset-react`, { 'development': true, minify: true }],
-  ];
-
-  const plugins = [
-    `@babel/plugin-syntax-dynamic-import`,
-  ];
-
-  return { presets, plugins };
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+      },
+    ],
+  },
 };
